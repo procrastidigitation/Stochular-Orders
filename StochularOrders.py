eaea752e7881fa9be1e3398ea4f7182d4915258f
Vishal Ashvinkumar <101580015+procrastidigitation@users.noreply.github.com>
2023-11-21 00:18:51 -0500

    Add files via upload

--- StochularOrders.py
+++ StochularOrders.py
@@ -13,9 +13,28 @@ import matplotlib.colors as mcolors
 
 
 class Distribution:
+    """
+    A class designed to numerically render probability distributions.
+    Default interpretation is a finite lottery.
+    """
     
     def __init__(self, vals, probs, precision=16):
-        """docstring"""
+        """
+        Initializes the *Distribution* object.
+        
+        Parameters
+        ----------
+        vals : list of numbers
+            Elements of the support of the *Distribution*.
+        probs : list of non-negative numbers
+            Probabilities with which values realize.
+            Must sum to 1.
+            Matched with values index-wise.
+        precision : int (optional)
+            Decimal precision for probabilities (default 16).
+            
+        Use *name*.dist to view the object in standard lottery notation: [(v1,p1), (v2,p2), ...].
+        """
         
         #Decimal precision for probs
         self.precision = precision
@@ -49,7 +68,9 @@ class Distribution:
 
         
     def renorm(self):
-        """docstring"""
+        """
+        Ensures *Distribution* object is initialized correctly.
+        """
         
         #Enforcing decimal precision
         self.probs = [round(p, self.precision) for p in self.probs]
@@ -63,7 +84,19 @@ class Distribution:
 
         
     def cdf(self, v):
-        """docstring"""
+        """
+        CDF representation of *Distribution*.
+        
+        Parameters
+        ----------
+        v : number
+            Value at which CDF of *Distribution* is evaluated.
+            
+        Returns
+        -------
+        float
+            Probability of *Distribution* realising at a value \leq v.
+        """
         
         for i in range(len(self.vals)-1, -1, -1):
             if self.vals[i] <= v:
@@ -72,7 +105,19 @@ class Distribution:
 
     
     def quantile(self, q):
-        """ docstring"""
+        """
+        Quantile representation of *Distribution*.
+        
+        Parameters
+        ----------
+        q : number between 0 and 1
+            Quantile at which value of *Distribution* is evaluated.
+            
+        Returns
+        -------
+        float
+            Value of *Distribution* at quantile q.
+        """
         
         for i in range(len(self.quants)):
             if self.quants[i] >= q:
@@ -80,27 +125,45 @@ class Distribution:
             
     
     def expectation(self):
-        """docstring"""
+        """
+        Returns the expected value of *Distribution*.
+        """
         
         weighted_vals = [self.vals[i]*self.probs[i] for i in range(len(self.vals))]
         return round(sum(weighted_vals), int(self.precision**0.5))
     
     
     def median(self):
-        """docstring"""
+        """
+        Returns the median value of *Distribution*.
+        """
         
         return self.quantile(0.5)
     
     
     def moment(self, n):
-        """docstring"""
+        """
+        Returns the n^{th} moment of *Distribution*.
+        
+        Parameters
+        ----------
+        n : int \geq 1
+            Integer degree of the moment being calculated.
+        """
         
         weighted_vals = [(self.vals[i]**n)*self.probs[i] for i in range(len(self.vals))]
         return round(sum(weighted_vals), int(self.precision**0.5))
     
     
     def central_moment(self, n):
-        """docstring"""
+        """
+        Returns the n^{th} central moment of *Distribution*.
+        
+        Parameters
+        ----------
+        n : int \geq 1
+            Integer degree of the central moment being calculated.
+        """
         
         E = self.expectation()
         weighted_vals = [((self.vals[i]-E)**n)*self.probs[i] for i in range(len(self.vals))]
@@ -108,21 +171,61 @@ class Distribution:
             
 
     def translate(F, t):
-        """docstring"""
+        """
+        Translates distribution F by shifter t.
+        This corresponds to left-right shifts of the CDF / up-down shifts of the quantile fn.
+        
+        Parameters
+        ----------
+        F : Distribution
+        t : number
+        
+        Returns
+        -------
+        Distribution
+            The *Distribution* for X+t, where X is a random variable distributed by F.
+        """
         
         vals = [v+t for v in F.vals]
         return Distribution(vals, F.probs, F.precision)
     
     
     def scale(F, c):
-        """docstring"""
+        """
+        Scales distribution F by factor c.
+        This corresponds to left-right stretches of the CDF / up-down stretches of the quantile fn.
+        
+        Parameters
+        ----------
+        F : Distribution
+        c : number
+            Can be negative, in which case the stretch also involves a flip.
+        
+        Returns
+        -------
+        Distribution
+            The *Distribution* for cX, where X is a random variable distributed by F.
+        """
         
         vals = [c*v for v in F.vals]
         return Distribution(vals, F.probs, F.precision)
     
     
     def indep_add(F, G):
-        """docstring"""
+        """
+        Independent addition of distributions F and G.
+        
+        Parameters
+        ----------
+        F : Distribution
+        G : Distribution
+        
+        Returns
+        -------
+        Distribution
+            The *Distribution* for X+Y, where X is a random variable distributed by F, Y is a random variable distributed by G, and X is independent to Y.
+            Equivalently: the convolution of F and G.
+        """
         
         vals = [v1+v2 for v1 in F.vals for v2 in G.vals]
         probs = [p1*p2 for p1 in F.probs for p2 in G.probs]
@@ -130,7 +233,20 @@ class Distribution:
     
     
     def comon_add(F, G):
-        """docstring"""
+        """
+        Comonotonic addition of distributions F and G.
+        
+        Parameters
+        ----------
+        F : Distribution
+        G : Distribution
+        
+        Returns
+        -------
+        Distribution
+            The *Distribution* for X+Y, where X is a random variable distributed by F, Y is a random variable distributed by G, and X is comonotonic to Y.
+            Equivalently: the *Distribution* with quantile F^{-1}+G^{-1}.
+        """
         
         quants = sorted(list(set(F.quants + G.quants)))
         quant_vals = [F.quantile(q) + G.quantile(q) for q in quants]
@@ -138,7 +254,20 @@ class Distribution:
     
     
     def mixture(F, G, alpha):
-        """docstring"""
+        """
+        Mixture between distributions F and G, with convex weight alpha placed on F.
+        
+        Parameters
+        ----------
+        F : Distribution
+        G : Distribution
+        alpha : number between 0 and 1
+        
+        Returns
+        -------
+        Distribution
+            The *Distribution* with CDF alpha*F + (1-alpha)*G.
+        """
         
         vals = F.vals + G.vals
         probs = [alpha*p for p in F.probs] + [(1.0-alpha)*p for p in G.probs]
@@ -146,7 +275,9 @@ class Distribution:
     
     
     def get_q_lub_ind(self, q):
-        """docstring"""
+        """
+        Memoisation device. No user interaction.
+        """
         
         if q not in self.lubs:
             i = 0
@@ -157,7 +288,21 @@ class Distribution:
     
     
     def integral(self, a, b):
-        """docstring"""
+        """
+        Calculates the integral from a to b for the quantile function of *Distribution*.
+        
+        Parameters
+        ----------
+        a : number between 0 and 1
+            Must be smaller than b.
+        b : number between 0 and 1
+            Must be larger than a.
+            
+        Returns
+        -------
+        float
+            \int_{a}^{b}F^{-1}(q)dq, where F is the CDF of *Distribution*.
+        """
         
         #Returns value of the quantile function if a==b
         if a == b:
@@ -180,10 +325,18 @@ class Distribution:
         #return l_margin + sum([(self.quants[i] - self.quants[i-1])*self.quant_vals[i] for i in range(l+1, r)]) + r_margin
         return round(l_margin + self.quints[(l+1,r)] + r_margin, int(self.precision**0.5))
     
-    
-    #Presently incomplete
+        
     def quantile_render(F, G):
-        """docstring"""
+        """
+        Generate graphics for a superimposition of the quantiles for distributions F and G.
+        
+        Parameters
+        ----------
+        F : Distribution
+        G : Distribution
+        """
+        
+        #Presently incomplete
         ax = plt.gca()
         
         plt.plot([0,1], [0,0], alpha=0.5, color='black', linewidth=0.5, linestyle=':')
@@ -206,9 +359,26 @@ class Distribution:
         
         
 class Bridge:
+    """
+    A class designed to numerically render any stochular bridge generated by intervallic systems.
+    """
     
     def __init__(self, phi, fineness=0.005):
-        """docstring"""
+        """
+        Initializes the *Bridge* object.
+        
+        Parameters
+        ----------
+        phi : function object taking in two floats a,b as arguments
+            Must be [0,1] valued.
+            For index i, the stochular order at i is identified with the system phi^{-1}([0,i]), where phi^{-1} denotes the preimage operation and [0,i] is a subinterval of [0,1].
+        fineness : float (optional)
+            Fineness of the triangular grid. (default 0.005).
+            For positive integers k and n such that (k+n)*fineness \leq 1, each gridpoint take the form (k*fineness, (k+n)*finenes).
+            Ideally divides 1.
+            
+        Spiritually this constructor works by defining a [0,1]-valued function over the triangle of intervals and identifying its lower-contour sets with systems.
+        """
         
         #Grid fineness
         self.fineness = fineness
@@ -218,13 +388,34 @@ class Bridge:
         
     
     def adjust_fineness(self, fineness):
-        """docstring"""
+        """
+        Sets the fineness of the triangular grid.
+        
+        Parameters
+        ----------
+        fineness : float
+            Fineness of the triangular grid.
+            For positive integers k and n such that (k+n)*fineness \leq 1, each gridpoint take the form (k*fineness, (k+n)*finenes).
+            Ideally divides 1.
+        """
         
         self.fineness = fineness
         
     
     def reach(self, F, G):
-        """docstring"""
+        """
+        Calculates the reach of distribution F over G along the stochular bridge *Bridge*.
+        
+        Parameters
+        ----------
+        F : Distribution
+        G : Distribution
+        
+        Returns
+        -------
+        float
+            The number \rho(F,G|*Bridge*).
+        """
         
         F = Distribution.scale(F, 1/self.fineness)
         G = Distribution.scale(G, 1/self.fineness)
@@ -265,7 +456,18 @@ class Bridge:
 
 
     def heatmap_render(F, G, fineness=0.005):
-        """docstring"""
+        """
+        Generate graphics for a heatmap of F.integral(a,b)-G.integral(a,b) over the triangle of intervals.
+        
+        Parameters
+        ----------
+        F : Distribution
+        G : Distribution
+        fineness : float
+            Fineness of the triangular grid.
+            For positive integers k and n such that (k+n)*fineness \leq 1, each gridpoint take the form (k*fineness, (k+n)*finenes).
+            Ideally divides 1.
+        """
         
         #Don't forget to zero out the diagonal
         d = int(1.0/fineness)
